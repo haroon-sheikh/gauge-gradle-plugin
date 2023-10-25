a5a3b8ce7b30717570e544efd2064ed823137945
Haroon Sheikh <haroon.sheikh@sky.uk>
2023-10-25 19:49:06 +0100

    Adds functionaltest sourceset with test suite plugin
    Signed-off-by: Haroon Sheikh <haroon.sheikh@outlook.com>
    Signed-off-by: Haroon Sheikh <haroon.sheikh@sky.uk>

---
+++ plugin/src/functionalTest/java/org/gauge/gradle/GaugePluginTest.java
@@ -0,0 +1,13 @@
+package org.gauge.gradle;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class GaugePluginTest {
+
+    @Test
+    void testMe() {
+        Assertions.assertTrue(true);
+    }
+
+}
