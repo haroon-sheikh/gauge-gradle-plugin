b574a8e24243b12dcebddfe10ef24050a2b9ee0b
Haroon Sheikh <haroon.sheikh@sky.uk>
2025-06-04 10:44:44 +0100

    Updating tasks so they're compatible with gradle 9

--- plugin/src/test/java/org/gauge/gradle/GaugePluginTest.java
+++ plugin/src/test/java/org/gauge/gradle/GaugePluginTest.java
@@ -1,13 +1,5 @@
 package org.gauge.gradle;
 
-import org.gradle.api.Project;
-import org.gradle.api.Task;
-import org.gradle.api.plugins.JavaPlugin;
-import org.gradle.api.tasks.TaskContainer;
-import org.gradle.testfixtures.ProjectBuilder;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-
 import static org.gauge.gradle.GaugeConstants.GAUGE_CLASSPATH_TASK;
 import static org.gauge.gradle.GaugeConstants.GAUGE_PLUGIN_ID;
 import static org.gauge.gradle.GaugeConstants.GAUGE_TASK;
@@ -15,7 +7,16 @@ import static org.gauge.gradle.GaugeConstants.GAUGE_TASK_GROUP;
 import static org.gauge.gradle.GaugeConstants.GAUGE_VALIDATE_TASK;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertInstanceOf;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.gradle.api.Project;
+import org.gradle.api.Task;
+import org.gradle.api.plugins.JavaPlugin;
+import org.gradle.api.tasks.TaskContainer;
+import org.gradle.testfixtures.ProjectBuilder;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 public class GaugePluginTest {
     private Project project;
@@ -30,7 +31,7 @@ public class GaugePluginTest {
         project.getPluginManager().apply(GAUGE_PLUGIN_ID);
         assertTrue(project.getPluginManager().hasPlugin("java"));
         assertTrue(project.getPluginManager().hasPlugin(GAUGE_PLUGIN_ID));
-        assertTrue(project.getPlugins().getPlugin(GAUGE_PLUGIN_ID) instanceof GaugePlugin);
+        assertInstanceOf(GaugePlugin.class, project.getPlugins().getPlugin(GAUGE_PLUGIN_ID));
         assertFalse(project.getPlugins().getPlugin(GAUGE_PLUGIN_ID) instanceof JavaPlugin);
     }
 
@@ -41,12 +42,12 @@ public class GaugePluginTest {
         var tasksMap = tasks.getAsMap();
         Task gauge = tasksMap.get(GAUGE_TASK);
         assertEquals(GAUGE_TASK_GROUP, gauge.getGroup());
-        assertTrue(gauge instanceof GaugeTask);
+        assertInstanceOf(GaugeTask.class, gauge);
         Task classpath = tasksMap.get(GAUGE_CLASSPATH_TASK);
         assertEquals(GAUGE_TASK_GROUP, classpath.getGroup());
-        assertTrue(classpath instanceof GaugeClasspathTask);
+        assertInstanceOf(GaugeClasspathTask.class, classpath);
         Task validate = tasksMap.get(GAUGE_VALIDATE_TASK);
         assertEquals(GAUGE_TASK_GROUP, validate.getGroup());
-        assertTrue(validate instanceof GaugeValidateTask);
+        assertInstanceOf(GaugeValidateTask.class, validate);
     }
 }
